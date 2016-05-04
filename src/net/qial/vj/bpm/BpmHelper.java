9e4031901be99b76d5dea49ac663d7a6d537c5d9
Kyle Watson <kw@qial.net>
2016-05-03 18:35:42 -0700

    Started work on a bpm based sequencer

---
+++ src/net/qial/vj/bpm/BpmHelper.java
@@ -0,0 +1,12 @@
+package net.qial.vj.bpm;
+
+/**
+ * Given a BPM and a framerate, this class uses the methods in BpmUtil to 
+ * provide simpler helper methods to calculate speed of travel.
+ * 
+ * @author kw
+ *
+ */
+public class BpmHelper {
+
+}
---
+++ src/net/qial/vj/sequencers/BpmPulseSequencer.java
@@ -0,0 +1,96 @@
+package net.qial.vj.sequencers;
+
+import net.qial.vj.sequencer.ProcessingSequencer;
+import net.qial.vj.util.DrawUtil;
+
+/**
+ * For now, BPM calculations with the sequencer are going to work
+ * on a strict multiplier rule.
+ * 
+ * pulseMultiplier is the amount of times a pulse runs in the BPM.
+ * This way effects that are exact factors of BPMs can be handled.
+ * 
+ * @author kw
+ *
+ */
+public class BpmPulseSequencer extends ProcessingSequencer {
+	// amount of frames it takes for the pulse to fully travel
+	protected int pulseMultiplier = 1;
+
+	// amount of frames the pulse takes to go up and back.
+	protected int pulseWidth = 20;
+
+	// internal vars
+	protected float pointWidth = 0.0f;
+
+	public BpmPulseSequencer() {
+		// pulses are in a single direction, so always positive
+		setPositive(true);
+		recalculate();
+	}
+
+	public float get(int point) {
+
+		int pointLocation = getPointLocation(point);
+		int pulseStart = getPulseStart();
+		int pulseEnd = getPulseEnd();
+		// println("point=" + point + " pointw=" + pointWidth + " pulseFrame="
+		// + pulseFrame + " position=" + pointLocation + " pStart="
+		// + pulseStart + " pEnd=" + pulseEnd);
+		if (pointLocation >= pulseStart && pointLocation <= pulseEnd) {
+			// get distance from pulse start
+			float dist = pointLocation - pulseStart;
+			// use the wave function
+			float amt = DrawUtil.wave(dist, pulseWidth);
+			println("dist=" + dist + " amt=" + amt);
+			return amt;
+		} else {
+			return 0.0f;
+		}
+
+	}
+
+	protected float getPulseLocation() {
+		// determine framecount to figure out what range
+		// of points are within the pulse
+		int pulseFrame = frameCount() % pulseLength;
+		// we need to adjust so the pulse starts before and
+		// ends after the start and end points of the effect
+		float pulseAdjustment = (pulseLength + pulseWidth)
+				/ ((float) pulseLength);
+		float pulseLocation = pulseFrame * pulseAdjustment
+				- (pulseWidth / 2.0f);
+		return pulseLocation;
+	}
+
+	protected int getPointLocation(int point) {
+		return round(pointWidth * point);
+	}
+
+	protected int getPulseStart() {
+		return round(getPulseLocation() - (pulseWidth / 2.0f));
+	}
+
+	protected int getPulseEnd() {
+		return round(getPulseLocation() + (pulseWidth / 2.0f));
+	}
+
+	public void setPulseLength(int pulseLength) {
+		this.pulseLength = pulseLength;
+		recalculate();
+	}
+	
+	public int getPulseLength() {
+		return pulseLength;
+	}
+	
+	public int getPulseWidth() {
+		return pulseWidth;
+	}
+
+	// recalculate internal variables
+	protected void recalculate() {
+		pointWidth = ((float) pulseLength) / ((float) getPoints());
+	}
+}
+
