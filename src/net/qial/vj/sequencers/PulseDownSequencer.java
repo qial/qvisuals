54ea03037fbadbc8a9ec74a2e3bcdcdc55b4b6c8
Kyle Watson <kw@qial.net>
2016-05-03 18:59:15 -0700

    Updated PulseDownSequencer to work right

--- src/net/qial/vj/sequencers/PulseDownSequencer.java
+++ src/net/qial/vj/sequencers/PulseDownSequencer.java
@@ -1,5 +1,7 @@
 package net.qial.vj.sequencers;
 
+import net.qial.vj.util.DrawUtil;
+
 /**
  * This class uses the same movement as the PulseSequencer, but all points
  * are at 1.0 at the start, and only move to 0.0 after the pulse peak passes.
@@ -7,5 +9,27 @@ package net.qial.vj.sequencers;
  *
  */
 public class PulseDownSequencer extends PulseSequencer {
+	public float get(int point) {
+		float pulseLocation = getPulseLocation();
 
+		int pointLocation = getPointLocation(point);
+		int pulseStart = getPulseStart();
+		int pulseEnd = getPulseEnd();
+		if(pointLocation >= pulseLocation) {
+			// if pulse hasn't passed, stay at 1.0
+			return 1.0f;
+		} else if (pointLocation >= pulseStart && pointLocation < pulseEnd) {
+			// pulse is going down
+			// get distance from pulse start
+			float dist = pointLocation - pulseStart;
+			// use the wave function
+			float amt = DrawUtil.wave(dist, pulseWidth);
+			println("dist=" + dist + " amt=" + amt);
+			return amt;
+		} else {
+			// return 0 because pulse has passed us
+			return 0.0f;
+		}
+
+	}
 }
